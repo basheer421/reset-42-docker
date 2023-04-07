03eb62a96001f883df65111364e4fe0d45894fb6
Basheer <109145360+basheer421@users.noreply.github.com>
2023-04-07 08:41:21 +0400

    Add files via upload

---
+++ reset_docker.sh
@@ -0,0 +1,6 @@
+rm -rf ~/.docker;
+rm -rf ~/Library/Containers/com.docker.docker;
+rm -rf ~/Library/Group\ Containers/group.com.docker;
+rm -rf ~/Library/Caches/com.docker.docker;
+rm -rf ~/Library/PrivilegedHelperTools/com.docker.vmnetd;
+rm -rf ~/Library/Preferences/com.docker.docker.plist;
